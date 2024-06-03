4b402725e6a971d1e5fa47f6e4178bfe87422b4c
Gabaruga <pinofag@gmail.com>
2024-06-03 20:24:09 +0300

    add player names input

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -22,9 +23,17 @@ func main() {
 	}
 
 	// Initialize our program
+	ti := textinput.New()
+	ti.Placeholder = "Player 1 name?"
+	ti.Focus()
+	ti.CharLimit = 20
+	ti.Width = 20
+
 	p := tea.NewProgram(model{
-		tick:   0,
-		player: 1,
+		tick:          0,
+		view:          0,
+		currentPlayer: 1,
+		textInput:     ti,
 		board: [][]int{
 			{0, 0, 0},
 			{0, 0, 0},
@@ -40,9 +49,15 @@ func main() {
 // program, so often it's a struct. For this simple example, however, all
 // we'll need is a simple integer.
 type model struct {
-	tick   int
-	board  [][]int
-	player int
+	tick          int
+	board         [][]int
+	currentPlayer int
+	player1_name  string
+	player1_score int
+	player2_name  string
+	player2_score int
+	view          int
+	textInput     textinput.Model
 }
 
 var pieces = map[int]rune{
@@ -61,14 +76,19 @@ func (m model) Init() tea.Cmd {
 	if m.tick > 0 {
 		return tick
 	}
+
+	if m.view == 0 {
+		return textinput.Blink
+	}
+
 	return nil
 }
 
 func updateCell(m *model, x int, y int) {
 	var cell = &m.board[x][y]
 	if *cell == 0 {
-		*cell = m.player
-		m.player *= -1
+		*cell = m.currentPlayer
+		m.currentPlayer *= -1
 	} else if *cell == 1 || *cell == -1 {
 		*cell *= -1
 	}
@@ -109,43 +129,61 @@ func updateCell(m *model, x int, y int) {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "q":
-			updateCell(&m, 0, 0)
-			return m, nil
-		case "w":
-			updateCell(&m, 0, 1)
-			return m, nil
-		case "e":
-			updateCell(&m, 0, 2)
-			return m, nil
-		case "a":
-			updateCell(&m, 1, 0)
-			return m, nil
-		case "s":
-			updateCell(&m, 1, 1)
-			return m, nil
-		case "d":
-			updateCell(&m, 1, 2)
-			return m, nil
-		case "z":
-			updateCell(&m, 2, 0)
-			return m, nil
-		case "x":
-			updateCell(&m, 2, 1)
-			return m, nil
-		case "c":
-			updateCell(&m, 2, 2)
-			return m, nil
-		case "esc":
-			m.board = [][]int{
-				{0, 0, 0},
-				{0, 0, 0},
-				{0, 0, 0},
+		switch m.view {
+		case 0:
+			switch msg.String() {
+			case "enter":
+				switch m.currentPlayer {
+				case 1:
+					m.player1_name = m.textInput.Value()
+					m.textInput.Placeholder = "Player 2 name?"
+					m.textInput.Reset()
+					m.currentPlayer *= -1
+				case -1:
+					m.player2_name = m.textInput.Value()
+					m.view = 1
+				}
+			default:
+				var cmd tea.Cmd
+				m.textInput, cmd = m.textInput.Update(msg)
+				return m, cmd
 			}
-
+		case 1:
+			switch msg.String() {
+			case "ctrl+c":
+				return m, tea.Quit
+			case "q":
+				updateCell(&m, 0, 0)
+			case "w":
+				updateCell(&m, 0, 1)
+			case "e":
+				updateCell(&m, 0, 2)
+			case "a":
+				updateCell(&m, 1, 0)
+			case "s":
+				updateCell(&m, 1, 1)
+			case "d":
+				updateCell(&m, 1, 2)
+			case "z":
+				updateCell(&m, 2, 0)
+			case "x":
+				updateCell(&m, 2, 1)
+			case "c":
+				updateCell(&m, 2, 2)
+			case "0":
+				m.view = 0
+			case "1":
+				m.view = 1
+			case "2":
+				m.view = 2
+			case "esc":
+				m.board = [][]int{
+					{0, 0, 0},
+					{0, 0, 0},
+					{0, 0, 0},
+				}
+			}
+			return m, nil
 		}
 	case tickMsg:
 		m.tick--
@@ -160,9 +198,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 // View returns a string based on data in the model. That string which will be
 // rendered to the terminal.
 func (m model) View() string {
-	// return fmt.Sprintf("Hi. This program will exit in %d seconds. To quit sooner press any key.\n", m)
-	// ┣ ╋ ┫ ━ ┃
-	return fmt.Sprintf("┏━┳━┳━┓\n┃%c┃%c┃%c┃\n┣━╋━╋━┫\n┃%c┃%c┃%c┃\n┣━╋━╋━┫\n┃%c┃%c┃%c┃\n┗━┻━┻━┛", pieces[m.board[0][0]], pieces[m.board[0][1]], pieces[m.board[0][2]], pieces[m.board[1][0]], pieces[m.board[1][1]], pieces[m.board[1][2]], pieces[m.board[2][0]], pieces[m.board[2][1]], pieces[m.board[2][2]])
+	v := "Tik-Tag-Go"
+	switch m.view {
+	case 0:
+		v = m.textInput.View()
+	case 1:
+		v = fmt.Sprintf("%s: %d\n%s: %d\n┏━┳━┳━┓\n┃%c┃%c┃%c┃\n┣━╋━╋━┫\n┃%c┃%c┃%c┃\n┣━╋━╋━┫\n┃%c┃%c┃%c┃\n┗━┻━┻━┛",
+			m.player1_name,
+			m.player1_score,
+			m.player2_name,
+			m.player2_score,
+			pieces[m.board[0][0]],
+			pieces[m.board[0][1]],
+			pieces[m.board[0][2]],
+			pieces[m.board[1][0]],
+			pieces[m.board[1][1]],
+			pieces[m.board[1][2]],
+			pieces[m.board[2][0]],
+			pieces[m.board[2][1]],
+			pieces[m.board[2][2]])
+	}
+	return v
 }
 
 // Messages are events that we respond to in our Update function. This
